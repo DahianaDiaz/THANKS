cfe2f755c568a2daf71c6acf369481d416404b36
DahianaDiaz <114367346+DahianaDiaz@users.noreply.github.com>
2022-11-04 20:29:21 -0400

    Add files via upload

---
+++ Scripts/menu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menu : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void botonStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void botonMenu()
+    {
+        SceneManager.LoadScene(0);
+   }
+    public void botonSalir()
+    {
+        Debug.Log("Quitamos el juego");
+        Application.Quit();
+    }
+}
+
